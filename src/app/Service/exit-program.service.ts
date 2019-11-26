bf05301af13cd4c2c07bb0282fa76c48cacf940d
AaronSeanego <draseanego@gmail.com>
2019-11-26 12:45:56 +0200

    Aaron

--- src/app/Service/exit-program.service.ts
+++ src/app/Service/exit-program.service.ts
@@ -12,52 +12,10 @@ export class ExitProgramService {
   constructor() {
   }
 
-<<<<<<< HEAD
   // generateKey() {
   //   let newKey = firebase.database().ref().child('Graduate/').push().key;
   //   return newKey;
   // }
-=======
-  signup(name,surname,gender,age,email,contact,address,qualification,status,price,password)
-  {
-   var db = firebase.firestore();
-​
-   return db.collection("Graduate/").add({
-     Username: name,
-     Surname: surname,
-     Email: email,
-     Gender: gender,
-     Age: age,
-     Contact: contact,
-     Address: address,
-     Qualification: qualification,
-     Status: status,
-     Price: price,
-     Password: password
-   }).then((docRef) => {
-     console.log("Document written with ID: ", docRef.id);
- }).catch((error) => {
-     console.error("Error adding document: ", error);
- });
-   
-  }
-​
- getGraduates() {
-   var db = firebase.firestore();
-   return db.collection("Graduates").get().then((querySnapshot) => {
-     querySnapshot.forEach((doc) => {
-         this.GraduatesArray.push(doc);
-     });
- });
-​
- return this.GraduatesArray;
- }
-
-  // signup(name,surname,email,contact,password)
-  //  {
-  //   var db = firebase.firestore();
-  //   console.log(name,email,password,"signup details serv")
->>>>>>> df6258e0b5717233cdcd212f77945d9bbc1188be
 
   signup(name,surname,gender,age,email,contact,address,qualification,category,status,price,password)
    {
@@ -82,23 +40,17 @@ export class ExitProgramService {
       console.error("Error adding document: ", error);
   });
     
-<<<<<<< HEAD
    }
 
-  getGraduates() {
-    var db = firebase.firestore();
-    return db.collection("Graduates/").get().then((querySnapshot) => {
-      querySnapshot.forEach((doc) => {
-          this.GraduatesArray.push(doc);
-        });
-    });
-  }
+  // getGraduates() {
+  //   var db = firebase.firestore();
+  //   return db.collection("Graduates/").get().then((querySnapshot) => {
+  //     querySnapshot.forEach((doc) => {
+  //         this.GraduatesArray.push(doc);
+  //       });
+  //   });
+  // }
 
   // return this.GraduatesArray;
   // }
-=======
-  //  }
-
-  
->>>>>>> df6258e0b5717233cdcd212f77945d9bbc1188be
 }
