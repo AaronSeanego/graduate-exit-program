d8c054f59bd69de7d3d34b19ef8d36122d67b006
AaronSeanego <draseanego@gmail.com>
2019-12-04 13:37:25 +0200

    Displayed the progress graph

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -117,43 +117,101 @@ try(){
   
   ngOnInit() {
 
-      // var Chart = require('chart.js');
-        var myChart = new Chart('myChart', {
-          type: 'line',
-          data: {
-              labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-              datasets: [{
-                  label: '# of Votes',
-                  data: [12, 19, 3, 5, 2, 3],
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)',
-                      'rgba(54, 162, 235, 0.2)',
-                      'rgba(255, 206, 86, 0.2)',
-                      'rgba(75, 192, 192, 0.2)',
-                      'rgba(153, 102, 255, 0.2)',
-                      'rgba(255, 159, 64, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255, 99, 132, 1)',
-                      'rgba(54, 162, 235, 1)',
-                      'rgba(255, 206, 86, 1)',
-                      'rgba(75, 192, 192, 1)',
-                      'rgba(153, 102, 255, 1)',
-                      'rgba(255, 159, 64, 1)'
-                  ],
-                  borderWidth: 1
+    var myChart = new Chart('StatsChart', {
+      type: 'line',
+      data: {
+          labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
+          datasets: [{
+              label: 'Carpenter',
+              data: [12, 19, 3, 5, 2, 3,42,54,12,21,23,65],
+              backgroundColor: [
+                  'rgba(8, 139, 247, 0)',
+                  'rgba(8, 139, 247, 0)',
+                  'rgba(8, 139, 247, 0)',
+                  'rgba(8, 139, 247, 0)',
+                  'rgba(8, 139, 247, 0)',
+                  'rgba(8, 139, 247, 0)'
+              ],
+              borderColor: [
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)',
+                  'rgba(177, 18, 23, 1)'
+              ],
+              borderWidth: 2
+          },{
+            label: 'Electrical Engineering',
+              data: [54, 75, 32, 32, 45, 12,65,5,17,32,15,43],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0)',
+                  'rgba(54, 162, 235, 0)',
+                  'rgba(255, 206, 86, 0)',
+                  'rgba(75, 192, 192, 0)',
+                  'rgba(153, 102, 255, 0)',
+                  'rgba(255, 159, 64, 0)'
+              ],
+              borderColor: [
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)',
+                  'rgba(8, 139, 247, 1)'
+              ],
+              borderWidth: 2
+          },{
+            label: 'Plumbing',
+              data: [12, 45, 55, 32, 1, 23,3,74,62,75,23,15],
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0)',
+                  'rgba(54, 162, 235, 0)',
+                  'rgba(255, 206, 86, 0)',
+                  'rgba(75, 192, 192, 0)',
+                  'rgba(153, 102, 255, 0)',
+                  'rgba(255, 159, 64, 0)'
+              ],
+              borderColor: [
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)',
+                  'rgba(227, 8, 247, 1)'
+              ],
+              borderWidth: 2
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
               }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero: true
-                      }
-                  }]
-              }
           }
-      });
+      }
+  });
+
     // var randomScalingFactor = function() {
 		// 	return Math.round(Math.random() * 100);
 		// };
@@ -340,120 +398,6 @@ try(){
         
         // marker.on('dragend', onDragEnd);
         this.MapboxService.run(this.pos);
-
-      //   var myChart = new Chart('myChart', {
-      //     type: 'line',
-      //     data: {
-      //         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      //         datasets: [{
-      //             label: '# of Votes',
-      //             data: [12, 19, 3, 5, 2, 3],
-      //             backgroundColor: [
-      //                 'rgba(255, 99, 132, 0.2)',
-      //                 'rgba(54, 162, 235, 0.2)',
-      //                 'rgba(255, 206, 86, 0.2)',
-      //                 'rgba(75, 192, 192, 0.2)',
-      //                 'rgba(153, 102, 255, 0.2)',
-      //                 'rgba(255, 159, 64, 0.2)'
-      //             ],
-      //             borderColor: [
-      //                 'rgba(255, 99, 132, 1)',
-      //                 'rgba(54, 162, 235, 1)',
-      //                 'rgba(255, 206, 86, 1)',
-      //                 'rgba(75, 192, 192, 1)',
-      //                 'rgba(153, 102, 255, 1)',
-      //                 'rgba(255, 159, 64, 1)'
-      //             ],
-      //             borderWidth: 1
-      //         }]
-      //     },
-      //     options: {
-      //         scales: {
-      //             yAxes: [{
-      //                 ticks: {
-      //                     beginAtZero: true
-      //                 }
-      //             }]
-      //         }
-      //     }
-      // });
-
-      var myChart = new Chart('CarpenterChart', {
-        type: 'line',
-        data: {
-            labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
-            datasets: [{
-                label: 'Carpenter',
-                data: [12, 19, 3, 5, 2, 3,42,54,12,21,23,65],
-                backgroundColor: [
-                    'rgba(8, 139, 247, 0)',
-                    'rgba(8, 139, 247, 0)',
-                    'rgba(8, 139, 247, 0)',
-                    'rgba(8, 139, 247, 0)',
-                    'rgba(8, 139, 247, 0)',
-                    'rgba(8, 139, 247, 0)'
-                ],
-                borderColor: [
-                    'rgba(177, 18, 23, 1)',
-                    'rgba(177, 18, 23, 1)',
-                    'rgba(177, 18, 23, 1)',
-                    'rgba(177, 18, 23, 1)',
-                    'rgba(177, 18, 23, 1)',
-                    'rgba(177, 18, 23, 1)'
-                ],
-                borderWidth: 2
-            },{
-              label: 'Electrical Engineering',
-                data: [54, 75, 32, 32, 45, 12,65,5,17,32,15,43],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0)',
-                    'rgba(54, 162, 235, 0)',
-                    'rgba(255, 206, 86, 0)',
-                    'rgba(75, 192, 192, 0)',
-                    'rgba(153, 102, 255, 0)',
-                    'rgba(255, 159, 64, 0)'
-                ],
-                borderColor: [
-                    'rgba(8, 139, 247, 1)',
-                    'rgba(8, 139, 247, 1)',
-                    'rgba(8, 139, 247, 1)',
-                    'rgba(8, 139, 247, 1)',
-                    'rgba(8, 139, 247, 1)',
-                    'rgba(8, 139, 247, 1)'
-                ],
-                borderWidth: 2
-            },{
-              label: 'Plumbing',
-                data: [12, 45, 55, 32, 1, 23,3,74,62,75,23,15],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0)',
-                    'rgba(54, 162, 235, 0)',
-                    'rgba(255, 206, 86, 0)',
-                    'rgba(75, 192, 192, 0)',
-                    'rgba(153, 102, 255, 0)',
-                    'rgba(255, 159, 64, 0)'
-                ],
-                borderColor: [
-                    'rgba(227, 8, 247, 1)',
-                    'rgba(227, 8, 247, 1)',
-                    'rgba(227, 8, 247, 1)',
-                    'rgba(227, 8, 247, 1)',
-                    'rgba(227, 8, 247, 1)',
-                    'rgba(227, 8, 247, 1)'
-                ],
-                borderWidth: 2
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
-    });
 
     }
 
